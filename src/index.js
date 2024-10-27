b3bd0a822123b829c8625b230218a3ce2c6a05f2
Darya Sivitskaya <dara.sivitskaya@gmail.com>
2024-10-27 17:23:36 +0100

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function toReadable (number) {
-  
+let numbers = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety"
+};
+    
+    function toReadable(number) {
+        if (number <= 20) {
+            return numbers[number];
+        }
+        if (number < 100) {
+            let tens = Math.floor(number / 10) * 10;
+            let units = number % 10;
+            return units !== 0 ? numbers[tens] + " " + numbers[units] : numbers[tens];
+        }
+        if (number < 1000) {
+            let hundred = Math.floor(number / 100);
+            let remainingValue = number % 100;
+            return remainingValue !== 0 ? numbers[hundred] + " hundred " + toReadable(remainingValue) : numbers[hundred] + " hundred";
+        }
+    }
+    return toReadable(number);
 }
